b2b55cbf65e62952cbd01887ce622aa56a794094
LNSEAB <lnseab@gmail.com>
2015-03-25 14:23:55 +0900

    MakeJointsプロジェクトの作成

---
+++ MakeJoints/MainClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MakeJoints
+{
+    public class MainClass :
+        PEPlugin.PEPluginClass
+    {
+        const string caption_ = "ジョイント生成フォームを開く";
+
+        public MainClass() :
+            base()
+        {
+            m_option = new PEPlugin.PEPluginOption( false, true, caption_ );
+        }
+
+        public override void Run(PEPlugin.IPERunArgs args)
+        {
+            try {
+                base.Run( args );
+            }
+            catch ( Exception e ) {
+                MessageBox.Show( e.Message, caption_, MessageBoxButtons.OK, MessageBoxIcon.Error );
+            }
+        }
+    }
+}
